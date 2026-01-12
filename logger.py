6977e24e300405e9c8bbff4089ead7b6d3cf602b
Ansh Singh <124670408+invinciblevenom@users.noreply.github.com>
2026-01-12 21:22:47 +0530

    Revert "Reduce logging"
    This reverts commit 31b797b73c281b4906f6fd5532228d64a2bfdf07.

--- logger.py
+++ logger.py
@@ -2,14 +2,15 @@ import logging
 import os
 from logging.handlers import RotatingFileHandler
 
+# removing old logs file if they exist.
 try:
     os.remove("logs.txt")
 except:
     pass
 
 logging.basicConfig(
-    level=logging.WARNING,
-    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(funcName)s() - Line %(lineno)d: %(name)s - %(message)s",
     datefmt="%d-%b-%y %I:%M:%S %p",
     handlers=[
         RotatingFileHandler("logs.txt", mode="w+", maxBytes=5000000, backupCount=5),
@@ -18,8 +19,7 @@ logging.basicConfig(
 )
 
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
-logging.getLogger("helpers.utils").setLevel(logging.ERROR)
-logging.getLogger("helpers.files").setLevel(logging.ERROR)
+
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
