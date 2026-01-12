93e5d24f77e39e31b0d6697b24f6fe36c407c0ea
Ansh Singh <124670408+invinciblevenom@users.noreply.github.com>
2026-01-12 14:46:26 +0530

    Store vars in env

--- config.py
+++ config.py
@@ -1,35 +1,13 @@
-# Copyright (C) @TheSmartBisnu
-# Channel: https://t.me/itsSmartDev
-
 from os import getenv
 from time import time
-from dotenv import load_dotenv
 
-try:
-    load_dotenv("config.env")
-except:
-    pass
-
-    if not getenv("BOT_TOKEN") or not getenv("BOT_TOKEN").count(":") == 1:
-        print("Error: BOT_TOKEN must be in format '123456:abcdefghijklmnopqrstuvwxyz'")
-        exit(1)
-
-    if (
-        not getenv("SESSION_STRING")
-        or getenv("SESSION_STRING") == "xxxxxxxxxxxxxxxxxxxxxxx"
-    ):
-        print("Error: SESSION_STRING must be set with a valid string")
-        exit(1)
-
-
-# Pyrogram setup
 class PyroConf(object):
-    API_ID = int(getenv("API_ID", "6"))
-    API_HASH = getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+    API_ID = int(getenv("API_ID"))
+    API_HASH = getenv("API_HASH")
     BOT_TOKEN = getenv("BOT_TOKEN")
     SESSION_STRING = getenv("SESSION_STRING")
+    
     BOT_START_TIME = time()
-
     MAX_CONCURRENT_DOWNLOADS = int(getenv("MAX_CONCURRENT_DOWNLOADS", "3"))
-    BATCH_SIZE = int(getenv("BATCH_SIZE", "10"))
+    BATCH_SIZE = int(getenv("BATCH_SIZE", "5"))
     FLOOD_WAIT_DELAY = int(getenv("FLOOD_WAIT_DELAY", "3"))
