1e01b6c66a8a324eba1a46751d37869404ead867
Ansh Singh <124670408+invinciblevenom@users.noreply.github.com>
2026-01-21 11:34:21 +0530

    Define upload limits

--- config.py
+++ config.py
@@ -9,5 +9,6 @@ class PyroConf(object):
     
     BOT_START_TIME = time()
     MAX_CONCURRENT_DOWNLOADS = int(getenv("MAX_CONCURRENT_DOWNLOADS", "3"))
+    MAX_CONCURRENT_UPLOADS = int(getenv("MAX_CONCURRENT_UPLOADS", "2"))
     BATCH_SIZE = int(getenv("BATCH_SIZE", "10"))
     FLOOD_WAIT_DELAY = int(getenv("FLOOD_WAIT_DELAY", "3"))
