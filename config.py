c106b5d1a57f540a09f5a47e5e66b964240acbc6
Ansh Singh <124670408+invinciblevenom@users.noreply.github.com>
2026-02-04 20:24:24 +0530

    Update config to 1

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class PyroConf(object):
     SESSION_STRING = getenv("SESSION_STRING")
     
     BOT_START_TIME = time()
-    MAX_CONCURRENT_DOWNLOADS = int(getenv("MAX_CONCURRENT_DOWNLOADS", "3"))
-    MAX_CONCURRENT_UPLOADS = int(getenv("MAX_CONCURRENT_UPLOADS", "2"))
-    BATCH_SIZE = int(getenv("BATCH_SIZE", "8"))
+    MAX_CONCURRENT_DOWNLOADS = int(getenv("MAX_CONCURRENT_DOWNLOADS", "1"))
+    MAX_CONCURRENT_UPLOADS = int(getenv("MAX_CONCURRENT_UPLOADS", "1"))
+    BATCH_SIZE = int(getenv("BATCH_SIZE", "1"))
     FLOOD_WAIT_DELAY = int(getenv("FLOOD_WAIT_DELAY", "10"))
